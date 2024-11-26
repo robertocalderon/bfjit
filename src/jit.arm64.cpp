2e3c2a42101b599d44ad1ab3802c468d4ebd66d7
Roberto Calderón Juárez <roberto.calderon.juarez@gmail.com>
2024-11-25 22:34:38 -0600

    jit: added support for arm64

--- src/jit.arm64.cpp
+++ src/jit.arm64.cpp
@@ -1,4 +1,3 @@
-
 #include "jit.hpp"
 #include "asmjit/a64.h"
 #include "asmjit/core/operand.h"
@@ -13,19 +12,21 @@ namespace a64 = asmjit::a64;
 constexpr auto DATA_BASE = a64::x7;
 constexpr auto DATA_INDEX = a64::x6;
 constexpr auto CACHE_VALUE = a64::x5;
+constexpr auto CACHE_VALUE_W = a64::w5;
 
 struct EHandler : public asmjit::ErrorHandler {
   void handleError(asmjit::Error err, char const *msg,
                    asmjit::BaseEmitter *) override {
-    fmt::print("asmjit error: {} ({})", msg, err);
+    fmt::print("asmjit error: {} ({})\n", msg, err);
     std::abort();
   }
 };
 
-void print_char(uint64_t r) {
+void print_char(uint32_t r) {
   r = r & 0xff;
   fmt::print("{}", char(r));
 }
+
 void outsize_of_bounds() {
   fmt::print("trying to access data outside of bouds\n");
 }
@@ -35,7 +36,6 @@ void do_codegen(asmjit::a64::Assembler &a, std::span<bfjit::BFOp const> code,
                 uint32_t data_size, std::vector<size_t> &jump_offsets);
 
 namespace bfjit {
-
 JIT::JIT(std::span<BFOp const> bytecode)
     : m_ip(0), m_ptr(0), m_bytecode(bytecode) {
   m_buffer.resize(4096);
@@ -55,8 +55,28 @@ void JIT::do_codegen() {
   auto exit_label = a.newLabel();
   auto outside_of_bounds = a.newLabel();
 
+  a.mov(DATA_BASE, a64::x0);
+  a.mov(DATA_INDEX, a64::x1);
+  a.mov(CACHE_VALUE, asmjit::Imm(0));
+
+  a.sub(a64::sp, a64::sp, asmjit::Imm(16));
+  a.str(a64::x29, a64::Mem(a64::sp, 0));
+  a.str(a64::x30, a64::Mem(a64::sp, 8));
+  a.mov(a64::x29, a64::sp);
+
+  // a.br(a64::x2);
+
   ::do_codegen(a, m_bytecode, exit_label, outside_of_bounds,
                (uint32_t)this->m_buffer.size(), this->mapping_bytecode_to_code);
+
+  a.bind(outside_of_bounds);
+
+  a.bind(exit_label);
+  a.mov(a64::sp, a64::x29);
+  a.ldr(a64::x29, a64::Mem(a64::sp, 0));
+  a.ldr(a64::x30, a64::Mem(a64::sp, 8));
+  a.add(a64::sp, a64::sp, asmjit::Imm(16));
+  a.ret(a64::x30);
 
   MFuncType func;
   auto const err = runtime.add(&func, &code_holder);
@@ -66,6 +86,7 @@ void JIT::do_codegen() {
   }
   this->main_function = func;
 }
+
 void JIT::run_until_end() {
   if (this->main_function) {
     // skip if already completed
@@ -90,17 +111,63 @@ void do_codegen(asmjit::a64::Assembler &a, std::span<bfjit::BFOp const> code,
     auto const &op = code[i];
     switch (op.m_type) {
     case bfjit::BFOp::Type::Mod:
-      std::abort();
+      jump_offsets.push_back(a.offset());
+      a.add(CACHE_VALUE, CACHE_VALUE, asmjit::Imm(op.inc_arg));
+      a.and_(CACHE_VALUE, CACHE_VALUE, asmjit::Imm(255));
+      break;
     case bfjit::BFOp::Type::ModPtr:
-      std::abort();
+      jump_offsets.push_back(a.offset());
+      a.strb(CACHE_VALUE_W, asmjit::a64::Mem(DATA_BASE, DATA_INDEX));
+      if (op.inc_ptr_arg < 0)
+        a.sub(DATA_INDEX, DATA_INDEX, asmjit::Imm(-op.inc_ptr_arg));
+      else
+        a.add(DATA_INDEX, DATA_INDEX, asmjit::Imm(op.inc_ptr_arg));
+      a.cmp(DATA_INDEX, asmjit::Imm(data_size));
+      a.b_gt(outside_bounds);
+      a.ldrb(CACHE_VALUE_W, asmjit::a64::Mem(DATA_BASE, DATA_INDEX));
+      break;
     case bfjit::BFOp::Type::Out:
-      std::abort();
-    case bfjit::BFOp::Type::LoopBeg:
-      std::abort();
-    case bfjit::BFOp::Type::LoopEnd:
-      std::abort();
+      jump_offsets.push_back(a.offset());
+      a.sub(a64::sp, a64::sp, asmjit::Imm(32));
+      a.str(CACHE_VALUE, a64::Mem(a64::sp, 0));
+      a.str(DATA_INDEX, a64::Mem(a64::sp, 8));
+      a.str(DATA_BASE, a64::Mem(a64::sp, 16));
+
+      a.mov(a64::x0, CACHE_VALUE);
+      a.bl(asmjit::Imm(print_char));
+
+      a.ldr(CACHE_VALUE, a64::Mem(a64::sp, 0));
+      a.ldr(DATA_INDEX, a64::Mem(a64::sp, 8));
+      a.ldr(DATA_BASE, a64::Mem(a64::sp, 16));
+      a.add(a64::sp, a64::sp, asmjit::Imm(32));
+      break;
+    case bfjit::BFOp::Type::LoopBeg: {
+      jump_offsets.push_back(a.offset());
+      auto end = a.newLabel();
+      auto start = a.newLabel();
+      a.bind(start);
+      a.cmp(CACHE_VALUE, 0);
+      a.b_eq(end);
+
+      loop_labels.push(start);
+      loop_labels.push(end);
+      break;
+    }
+    case bfjit::BFOp::Type::LoopEnd: {
+      auto end = loop_labels.top();
+      loop_labels.pop();
+      auto start = loop_labels.top();
+      loop_labels.pop();
+      jump_offsets.push_back(a.offset());
+      a.b(start);
+      a.bind(end);
+      break;
+    }
+
     case bfjit::BFOp::Type::SetValue:
-      std::abort();
+      jump_offsets.push_back(a.offset());
+      a.mov(CACHE_VALUE, asmjit::Imm(op.set_arg));
+      break;
     case bfjit::BFOp::Type::In:
       std::abort();
     case bfjit::BFOp::Type::Halt:
